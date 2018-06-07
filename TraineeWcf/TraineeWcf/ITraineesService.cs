c44ac4cea987e5a9b1af8d0305983947a384f0ea
Shishika <Shishika.Bethireddy@neudesic.com>
2018-06-07 20:39:04 +0530

    adding wcf

---
+++ TraineeWcf/TraineeWcf/ITraineesService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TraineeWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITraineesService" in both code and config file together.
+    [ServiceContract]
+    public interface ITraineesService
+    {
+        [OperationContract]
+        List<Trainees> GetTrainee();
+
+        [OperationContract]
+        List<Trainees> AddTraniee(Trainees addNewTraine);
+
+        [OperationContract]
+        List<Trainees> UpdateTrainee(Trainees updatedTrainee);
+
+        [OperationContract]
+        List<Trainees> DeleteTrainee(int deletedTraineeId);
+    }
+
+    [DataContract]
+    public class Trainees
+    {
+        [DataMember]
+        public int TraineeId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ TraineeWcf/TraineeWcf/TraineesService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TraineeWcf
+{
+    public class TraineesService : ITraineesService
+    {
+       public List<Trainees> trainees = new List<Trainees>()
+        {
+            new Trainees {TraineeId=1,Name="renu",Age=20,DOB=new DateTime(1997,4,5) },
+            new Trainees {TraineeId=2,Name="shi",Age=20,DOB=new DateTime(1997,6,30) }
+        };
+        public List<Trainees> AddTraniee(Trainees addNewTrainee)
+        {
+            trainees.Add(addNewTrainee);    
+            return trainees;
+        }
+        public List<Trainees> DeleteTrainee(int deletedTraineeId)
+        {
+            var deletingTrainee = trainees.Find(t => t.TraineeId == deletedTraineeId);
+            trainees.Remove(deletingTrainee);
+            return trainees;
+        }
+        public List<Trainees> GetTrainee()
+        {
+            return trainees;
+        }
+        public List<Trainees> UpdateTrainee(Trainees updatedTrainee)
+        {
+            var deletingTrainee = trainees.Find(t => t.TraineeId == updatedTrainee.TraineeId);
+            if (deletingTrainee == null)
+                trainees.Add(updatedTrainee);        
+            else
+            {
+                trainees.Remove(deletingTrainee);
+                trainees.Add(updatedTrainee);
+            }
+            return trainees;
+            //throw new NotImplementedException();
+        }
+    }
+}
